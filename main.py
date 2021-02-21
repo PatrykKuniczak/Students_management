6706cc6815cba5027c5e8692bdeaf13f1977c22d
PatrykKuniczak <p.kuniczak@gmail.com>
2021-02-21 20:42:40 +0100

    Commit with finished student adding

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from math import ceil
 
 
 class Student:
-    def __init__(self, first_name, last_name, birthday, journal_number=0):
+    def __init__(self, first_name, last_name, birthday):
         self.birthday = birthday
-        self.journal_number = journal_number
         self.first_name = first_name
         self.last_name = last_name
         self.semester = 1
-        self.student_list = {}
-        self.student_date = []
+        self.student_date_list = []
 
     def go_high(self):
         self.semester += 1
@@ -22,43 +20,43 @@ class Student:
         return ceil(self.semester / 2)
 
     def join_a_student(self):
-        self.student_date.append(f"Imię:{self.first_name}, Nazwisko:{self.last_name}, Data Urodzenia:{self.birthday}")
-
-        self.student_list.update({self.journal_number: self.student_date})
-
-        return self.student_list
+        self.student_date_list.append(f"Imię:{self.first_name}, "
+                                      f"Nazwisko:{self.last_name}, Data Urodzenia:{self.birthday}")
+        return self.student_date_list
 
     # def print_a_student(self):
     #     for elem in self.student_date:
     #         print(f"Nr. {self.journal_number}: {elem}")
 
 
-student_list = {}
+student_dict = {}
 
 
-def make_a_new_student():
+def make_a_new_student(student_journal_index):
     while True:
         try:
             new_student_first_name = input("Podaj imię: ").strip()
             new_student_last_name = input("Podaj nazwisko: ").strip()
             new_student_birthday = input("Podaj datę urodzenia[dd.mm.yyyy]: ")
             datetime.strptime(new_student_birthday, '%d.%m.%Y')
-            new_student_journal_number = int(input("Podaj numer do dziennika: "))
             break
         except ValueError:
             print("Któraś z podanych warości jest nieprawidłowa!")
 
     student = Student(new_student_first_name, new_student_last_name,
-                      new_student_birthday, new_student_journal_number)
+                      new_student_birthday)
     list_element = student.join_a_student()
-    student_list.update(list_element)
+    student_dict[student_journal_index] = list_element
 
+
+student_index = 0
 
 while True:
-    question_about_add = input("Czy chcesz dodać studenta: ").strip().lower()
+    student_index += 1
+    question_about_add = input("Czy chcesz dodać studenta[Tak/Nie]: ").strip().lower()
     if question_about_add == "tak":
-        make_a_new_student()
+        make_a_new_student(student_index)
     elif question_about_add == "nie":
         break
 
-print(f"Oto lista studentów: {student_list}")
+print(f"Oto lista studentów: {student_dict}")
