017723a66466ba7d58797be16c899686555a00df
PatrykKuniczak <p.kuniczak@gmail.com>
2021-02-21 21:25:57 +0100

    Commit with syntax refactor

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ class Student:
         self.birthday = birthday
         self.first_name = first_name
         self.last_name = last_name
+
         self.semester = 1
-        self.student_date_list = []
+        self.student_data_list = []
 
     def go_high(self):
         self.semester += 1
@@ -19,16 +20,16 @@ class Student:
     def get_year(self):
         return ceil(self.semester / 2)
 
-    def join_a_student(self):
-        self.student_date_list.append(f"Imię:{self.first_name}, "
+    def make_a_student(self):
+        self.student_data_list.append(f"Imię:{self.first_name}, "
                                       f"Nazwisko:{self.last_name}, Data Urodzenia:{self.birthday}")
-        return self.student_date_list
+        return self.student_data_list
 
 
 student_dict = {}
 
 
-def make_a_new_student(student_journal_index):
+def data_for_new_student(student_journal_index):
     while True:
         try:
             new_student_first_name = input("Podaj imię: ").strip()
@@ -41,7 +42,7 @@ def make_a_new_student(student_journal_index):
 
     student = Student(new_student_first_name, new_student_last_name,
                       new_student_birthday)
-    list_element = student.join_a_student()
+    list_element = student.make_a_student()
     student_dict[student_journal_index] = list_element
 
 
@@ -51,7 +52,7 @@ while True:
     student_index += 1
     question_about_add = input("Czy chcesz dodać studenta[Tak/Nie]: ").strip().lower()
     if question_about_add == "tak":
-        make_a_new_student(student_index)
+        data_for_new_student(student_index)
     elif question_about_add == "nie":
         break
 
