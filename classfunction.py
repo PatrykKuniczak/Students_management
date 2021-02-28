442d33e3594f01a9f18d23233320a888263bc8f7
PatrykKuniczak <p.kuniczak@gmail.com>
2021-03-01 00:49:05 +0100

    Commit after add logging

---
+++ classfunction.py
@@ -0,0 +1,12 @@
+# class Class:
+#     def __init__(self):
+#         self.
+
+
+"""
+1.Display całej klasy w której są studenci
+2.Obiekt ma zawiera nazwę i obecny rocznik studentów,
+3.Edycja klas nazw oraz studentów z danej klasy
+4.Usuwanie studentów z danej klasy
+5.Usuwanie klas
+"""
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from function import *
+from studentfunction import general
 
 # TODO: ZRÓB DEKORATOR KTÓRY OTWIERA I ZAMYKA JSONA
 def main() -> None:
-    general()
+    while True:
+        generalfunc = general()
+        if not generalfunc:
+            break
 
 if __name__ == '__main__':
     main()
